d5b767008cd28c77d2009476ef9487fd1077261d
Timo Walter <timo@unicorn-development.de>
2024-09-20 23:21:16 +0200

    adjust existing tests

--- lib/workers/repository/onboarding/pr/index.spec.ts
+++ lib/workers/repository/onboarding/pr/index.spec.ts
@@ -33,6 +33,7 @@ describe('workers/repository/onboarding/pr/index', () => {
       branches = [];
       platform.massageMarkdown.mockImplementation((input) => input);
       platform.createPr.mockResolvedValueOnce(partial<Pr>());
+      platform.maxBodyLength.mockImplementationOnce(() => Infinity);
       GlobalConfig.reset();
     });
 
