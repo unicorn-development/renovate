9b74c98127b108aefedf2603b90ff2f7cbb72b16
zivillian <zivillian@users.noreply.github.com>
2024-10-29 11:16:53 +0100

    add sign to repo list

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
     ":prHourlyLimitNone",
     ":prConcurrentLimitNone",
   ],
-  repositories: ["Development/WorkSimple.Houston", "Development/hshl.IDM", "Development/hshl.myHSHL", "Development/hshl.select"],
+  repositories: ["Development/WorkSimple.Houston", "Development/hshl.IDM", "Development/hshl.myHSHL", "Development/hshl.select", "Development/hshl.sign"],
   packageRules: [
     {
       matchSourceUrls: [
