b6a6af1cfa5903da02b9904fc1ae2175f1617bdd
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2026-06-25 13:56:36 +0200

    Update config.js (#1)

--- config.js
+++ config.js
@@ -9,7 +9,18 @@ module.exports = {
     ":prHourlyLimitNone",
     ":prConcurrentLimitNone",
   ],
-  repositories: ["Development/WorkSimple.Houston", "Development/hshl.IDM", "Development/hshl.myHSHL", "Development/hshl.select", "Development/hshl.sign", "Development/bfw.IDM", "Development/bfw.Portal", "Development/Stromodul", "Development/stg.Wohlbefindlichkeit"],
+  repositories: [
+    "Development/WorkSimple.Houston",
+    "Development/hshl.IDM",
+    "Development/hshl.myHSHL",
+    "Development/hshl.select",
+    "Development/hshl.sign",
+    "Development/bfw.IDM",
+    "Development/bfw.Portal",
+    "Development/Stromodul",
+    "Development/stg.Wohlbefindlichkeit",
+    "Development/Connext.VivendiSDK"
+  ],
   packageRules: [
     {
       matchSourceUrls: [
